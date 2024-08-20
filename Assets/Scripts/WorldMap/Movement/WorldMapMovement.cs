c83161a75690b6fef8c3c6d2692c84b806c48623
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-08-20 10:59:43 -0400

    basic map movement is functioning
    still need to add rotation for it

--- Assets/Scripts/WorldMap/Movement/WorldMapMovement.cs
+++ Assets/Scripts/WorldMap/Movement/WorldMapMovement.cs
@@ -9,14 +9,40 @@ public class WorldMapMovement : MonoBehaviour
     private float horizontal;
     private float vertical;
     public float speed = 5f;
+    public float gravity = -40f;
+    public float jumpPower = 15f;
+    public float groundDistance = 0.4f;
+    public Transform groundCheck;
+    public LayerMask ground;
 
     //Additional settings values.
     public float normalSpeed = 5f;
     public float sprintSpeed = 10f;
+    public bool canMove = true;
+    private bool canJump = true;
 
+    //random important values
+    private Vector3 velocity = new Vector3(0,0,0);
+    private bool isGrounded; 
+    
     // Update is called once per frame
     void Update()
     {
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+            canJump = true;
+        }
+
+        if(Input.GetButtonDown("Jump") && canJump)
+        {
+            velocity.y = jumpPower;
+            canJump = false;
+        }
+
         //Get the input of the player.
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
@@ -34,6 +60,15 @@ public class WorldMapMovement : MonoBehaviour
         }
 
         //Do the actual movement.
-        transform.position = new Vector3(horizontal,0,vertical) * speed * Time.deltaTime;
+        transform.Translate(new Vector3(horizontal,0,vertical) * speed * Time.deltaTime);
+
+        //Do gravity
+        if(canMove)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        
+        transform.Translate(velocity * Time.deltaTime);
+
     }
 }
