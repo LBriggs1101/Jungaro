b3e950d919cda543612767a6d4d4f7811a87a710
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-09-09 09:09:18 -0400

    asdf
    asdf

---
+++ Assets/Scripts/DeathStuff/DeathManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject deathScreen;
+    public int numOfDeaths;
+
+    public void death()
+    {
+
+    }
+
+}
