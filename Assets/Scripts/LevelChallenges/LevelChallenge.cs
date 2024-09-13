d2d00cf0ad856f1a8e1a1dd5bc1a9b3f49761397
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-09-13 08:00:33 -0400

    challenge manager done
    asdf

---
+++ Assets/Scripts/LevelChallenges/LevelChallenge.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChallenge : MonoBehaviour
+{
+    public virtual bool beatChallengeValue()
+    {
+        return false;
+    }
+}
---
+++ Assets/Scripts/LevelChallenges/TestLevelChallenge.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLevelChallenge : LevelChallenge
+{
+    public override bool beatChallengeValue()
+    {
+        return true;
+    }
+}
