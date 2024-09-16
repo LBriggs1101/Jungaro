f056a3c1b0c02a83283805967b12d8ee65696f54
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-09-15 21:29:50 -0400

    Merge remote-tracking branch 'origin/Nathaniel' into Prod

---
+++ Assets/New Stuff/ScottCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScottCamera : MonoBehaviour
+{
+    public float sensitivity;
+    public Quaternion rotationy = Quaternion.identity;
+    public Vector2 turn;
+
+
+    void Update()
+    {
+
+        //Gets horizontal and vertical axis
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        //Gets mouse input for camera
+        turn.x = Input.GetAxis("Mouse X");
+        turn.y = Input.GetAxis("Mouse Y");
+
+        //Rotation for player and camera
+        if (turn.x != 0 || turn.y != 0) {
+
+            //Calculates amount of turn needed
+            rotationy.y += turn.y * sensitivity * Time.deltaTime;
+            rotationy.x += turn.x * sensitivity * Time.deltaTime;
+
+            //Prevents camera overcorrecting
+            rotationy.y = Mathf.Clamp(rotationy.y, -35, 25);
+
+            //Rotates player accordingly
+            transform.Rotate(Vector3.up * turn.x * sensitivity * Time.deltaTime);
+
+            //Rotate camera to match mouse
+            transform.GetChild(0).rotation = Quaternion.Euler(rotationy.y * -1, rotationy.x, transform.GetChild(0).rotation.z);
+        }
+        
+    }
+}
